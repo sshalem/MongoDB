8c117945c3e0ac1f7a59e804ee726b670ee046f5
sshalem <shabtay.shalem@gmail.com>
2018-06-21 10:58:15 +0300

    2018-06-21 version

---
+++ CouponManageSystem_2018_06_21/src/main/java/shabtay/coupon/system/common/CouponType.java
@@ -0,0 +1,18 @@
+package shabtay.coupon.system.common;
+
+/**
+ * ENUM defines 7 type of coupon options : RESTURANTS, ELECTRICITY, FOOD, HEALTH, SPORTS, CAMPING ,TRAVELLING
+ * @author User
+ * 
+ */
+public enum CouponType {
+
+	RESTURANTS,
+	ELECTRICITY,
+	FOOD,
+	HEALTH,
+	SPORTS,
+	CAMPING,
+	TRAVELLING
+	
+}
