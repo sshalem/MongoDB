ed147926ad515472630878fcffe949c16dc12cf3
sshalem <shabtay.shalem@gmail.com>
2018-07-19 01:26:24 +0300

    with redis

---
+++ CouponManageSystem20180718withRedis/src/main/java/shabtay/coupon/system/CouponManageSystemApplication.java
@@ -0,0 +1,14 @@
+package shabtay.coupon.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class CouponManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponManageSystemApplication.class, args);
+	}
+}
